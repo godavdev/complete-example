80711114a676442eb3d23b82d3342fb7ff5039e7
godavdev <godavdeveloper@gmail.com>
2026-05-05 14:50:25 -0600

    feat: restructure app initialization for improved readability and maintainability

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,9 +1,19 @@
 import { cors } from "@elysiajs/cors"
 import { Elysia } from "elysia"
 import { auth } from "./auth"
+import { posts } from "./posts"
+import { users } from "./users"
 
-const app = new Elysia().use(cors()).use(auth).listen(3000)
+const app = new Elysia()
+  .use(cors())
+  .use(auth)
+  .use(posts)
+  .use(users)
+  .listen(3000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 )
+
+// For threaty
+export type App = typeof app
