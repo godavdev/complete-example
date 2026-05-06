0b43ca97d08b79397dfa25faffc158949f2be621
godavdev <dgaduff@gmail.com>
2026-05-06 00:13:21 -0600

    feat(shared): remove unused service exports from index file

--- apps/web/services/index.ts
+++ apps/web/services/index.ts
@@ -1,16 +1,7 @@
 import { initServices } from "@repo/shared"
+
 export const services = initServices("http://localhost:8000", {
   fetch: {
     credentials: "include",
   },
 })
-export const {
-  posts: postService,
-  users: { findById: findUserByIdService },
-} = services
-
-export const {
-  create: createPostService,
-  list: listPostsService,
-  delete: deletePostService,
-} = postService
