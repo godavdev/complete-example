2e35abf3c21041e72519ac57ed090338f5f6a164
godavdev <godavdeveloper@gmail.com>
2026-05-05 18:30:50 -0600

    feat(auth): remove unused auth mutation and query options

--- apps/web/feats/auth/auth-queries.ts
+++
@@ -1,9 +0,0 @@
-import { queryOptions } from "@tanstack/react-query"
-import { getCurrentUser } from "./auth-services"
-
-export const signInOptions = queryOptions({
-  queryKey: [
-    "auth",
-  ],
-  queryFn: getCurrentUser,
-})
