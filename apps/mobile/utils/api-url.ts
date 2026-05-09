d1e0ef2032f317d769889d768e8e41cb72876e63
godavdev <godavdeveloper@gmail.com>
2026-05-09 12:37:03 -0600

    fix(api): update apiUrl logic to use AND condition for environment checks; add .env.example for backend configuration

--- apps/mobile/utils/api-url.ts
+++ apps/mobile/utils/api-url.ts
@@ -2,9 +2,7 @@
 import Constants from "expo-constants"
 
 export const apiUrl =
-  Constants.executionEnvironment === "storeClient" ||
-  Constants.executionEnvironment === "bare"
-    ? Constants.expoConfig?.hostUri
-      ? `http://${Constants.expoConfig?.hostUri?.split(":")[0]}:${process.env.EXPO_PUBLIC_LOCAL_BACKEND_PORT ?? 8000}`
-      : (process.env.EXPO_PUBLIC_BACKEND_URL ?? "")
+  Constants.executionEnvironment === "storeClient" &&
+  Constants.expoConfig?.hostUri
+    ? `http://${Constants.expoConfig?.hostUri?.split(":")[0]}:${process.env.EXPO_PUBLIC_LOCAL_BACKEND_PORT ?? 8000}`
     : (process.env.EXPO_PUBLIC_BACKEND_URL ?? "")
