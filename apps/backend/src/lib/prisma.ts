991e4a82be33fe4a599aaa4767f5fefc8f3d3cad
godavdev <godavdeveloper@gmail.com>
2026-05-25 12:10:18 -0600

    feat(docker): add Docker Compose setup and PostgreSQL integration for backend

--- apps/backend/src/lib/prisma.ts
+++ apps/backend/src/lib/prisma.ts
@@ -1,22 +1,27 @@
-import { PrismaLibSql } from "@prisma/adapter-libsql";
-import { PrismaClient } from "../generated/prisma/client";
+// import { PrismaLibSql } from "@prisma/adapter-libsql";
+import { PrismaPg } from "@prisma/adapter-pg"
+import { PrismaClient } from "../generated/prisma/client"
 
-const adapter = new PrismaLibSql({
-  url: process.env.DATABASE_URL ?? "",
-});
+// const adapter = new PrismaLibSql({
+//   url: process.env.DATABASE_URL ?? "",
+// });
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL ?? "",
+})
 
 const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
-};
+  prisma: PrismaClient
+}
 
 const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     adapter,
-  });
+  })
 
 if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
+  globalForPrisma.prisma = prisma
 }
 
-export { prisma };
+export { prisma }
